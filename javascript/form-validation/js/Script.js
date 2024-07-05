520a68f74f11d80d4229e6b18c2f46caedfd8828
krishna-412web <krishnarenjith@techversantinfotech.com>
2024-07-05 17:35:46 +0530

    updated script.js

--- javascript/form-validation/js/Script.js
+++ javascript/form-validation/js/Script.js
@@ -3,11 +3,13 @@ const lastName = document.getElementById("last-name");
 const email = document.getElementById("email");
 const phone = document.getElementById("phone-number");
 const password= document.getElementById("new-password");
-const s = document.getElementById("male");
-const s1 = document.getElementById("female");
-const ms = document.getElementByName("unmarried");
-const ms1 = document.getElementByName("married")
-
+const s = document.getElementsByName("gender");
+const ms = document.getElementsByName("m-status");
+const pic = document.getElementById("profile-picture");
+const date = document.getElementById("dob");
+const occup = document.getElementById("referrer");
+const bio = document.getElementById("bio");
+const terms = document.getElementsByName("terms-and-condition");
 
 const fnameError = document.getElementById("fname-error");
 const lnameError = document.getElementById("lname-error");
@@ -16,12 +18,20 @@ const phError = document.getElementById("num-error");
 const passError = document.getElementById("pass-error");
 const sError = document.getElementById("s-error");
 const msError = document.getElementById("ms-error");
+const picError = document.getElementById("pic-error");
+const dateError = document.getElementById("dob-error");
+const occupError = document.getElementById("ref-error");
+const bioError = document.getElementById("bio-error");
+const termsError = document.getElementById("terms-error");
 
 const form = document.getElementById("form");
 
-let email_check = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+let email_check = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z])$/;
 let pass_check = /^([A-Za-z0-9*#_])$/
 
+let b1=0;
+let b2=0;
+let b3=0;
 form.addEventListener('submit',(e) =>
 {
 	let valid = true; 
@@ -80,8 +90,13 @@ form.addEventListener('submit',(e) =>
 	{
 		passError.innerText = "";
 	}		
-
-	if(s.value == "" || s.value == null)
+	
+	for (let i = 0; i < s.length; i++) {
+		if (s[i].checked) {
+    			b1+=1;
+  		}
+	}
+	if(b1 == 0)
 	{
 		e.preventDefault();
 		sError.innerText = "*Atleast one button should be checked";
@@ -92,7 +107,12 @@ form.addEventListener('submit',(e) =>
 		sError.innerText = "";
 	}	
 
-	if(ms.value == "" || ms.value == null)
+	for (let i = 0; i < ms.length; i++) {
+		if (ms[i].checked) {
+    			b2+=1;
+  		}
+	}
+	if(b1 == 0)
 	{
 		e.preventDefault();
 		msError.innerText = "*Atleast one button should be checked";
@@ -102,6 +122,67 @@ form.addEventListener('submit',(e) =>
 	{
 		msError.innerText = "";
 	}
+
+	if(pic.value == '' || pic.value == null)
+	{
+		e.preventDefault();
+		picError.innerText = "*file is not uploaded";
+		valid = false;
+	}
+	else
+	{
+		picError.innerText = "";
+	}
+
+	if(date.value == '' || date.value == null)
+	{
+		e.preventDefault();
+		dateError.innerText = "*date field is required";
+		valid = false;
+	}
+	else
+	{
+		dateError.innerText = "";
+	}
+
+	if(occup.value == '')
+	{
+		e.preventDefault();
+		occupError.innerText = "*Atleast select one option";
+		valid = false;
+	}
+	else
+	{
+		occupError.innerText = "";
+	}
+	
+	if(bio.value == '' || bio.value == null)
+	{
+		e.preventDefault();
+		bioError.innerText = "*Bio is required";
+		valid = false;
+	}
+	else
+	{
+		bioError.innerText = "";
+	}
+	
+	for (let i = 0; i < terms.length; i++) {
+		if (terms[i].checked) {
+    			b3+=1;
+  		}
+	}
+	if(b3 == 0)
+	{
+		e.preventDefault();
+		termsError.innerText = "*Atleast one button should be checked";
+		valid = false;
+	}
+	else
+	{
+		termsError.innerText = "";
+	}
+		
 
 	window.scrollTo(0,0);
 	if(!valid)
